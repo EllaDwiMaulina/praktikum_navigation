14882d2436023a8ac9b448bf95b68ac95b8b5954
Ella Maulina <theyluvwin28@gmail.com>
2024-10-01 22:10:52 +0700

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:belanja/models/item.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  final List<Item> items = [
+    Item('Bracelet', 185000, 'assets/bracelet.jpeg',),
+    Item('Glasses', 300000, 'assets/glasses.png'),
+    Item('Handbag', 600000, 'assets/handbag.jpg'),
+    Item('Watch Stylish', 350000, 'assets/watch_stylish.jpeg'),
+  ];
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Daftar Barang Belanja'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  '/item',
+                  arguments: items[index],
+                );
+              },
+
+              
+              child: ListTile(
+                title: Text(items[index].name),
+                subtitle: Text('Rp ${items[index].price}'),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
